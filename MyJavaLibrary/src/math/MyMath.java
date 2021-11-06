0cfe038cf253b0f8d9222d02923ee103987cb978
OleinikIgnat <oda33255342@gmail.com>
2021-11-06 10:32:37 +0200

    add MyLibrary

---
+++ MyJavaLibrary/src/math/MyMath.java
@@ -0,0 +1,9 @@
+package math;
+
+public class MyMath {
+    public static double calcCircleArea(double r)
+    {
+        double Area = Math.PI * r * r;
+        return Area;
+    }
+}
---
+++ MyMathFunctionsTester/src/mymathfunctionstester/MyMathFunctionsTester.java
@@ -0,0 +1,13 @@
+package mymathfunctionstester;
+
+import math.MyMath;
+
+public class MyMathFunctionsTester {
+
+    public static void main(String[] args) {
+        double R = 3;
+        double S = MyMath.calcCircleArea(R);
+        System.out.println(S);
+    }
+    
+}
